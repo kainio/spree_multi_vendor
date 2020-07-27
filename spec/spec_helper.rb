9dce71cece87379612053edc421ecd53efacbf0e
Soufiane El Kassib <poliamus@yahoo.com>
2020-07-27 19:24:49 +0100

    initial commit

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,6 +6,8 @@ require File.expand_path('../dummy/config/environment.rb', __FILE__)
 require 'shoulda/matchers'
 require 'spree_dev_tools/rspec/spec_helper'
 
+# Selenium::WebDriver::Chrome::Service.driver_path = "/mnt/c/Users/soufi/bin/chromedriver.exe"
+
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
 Dir[File.join(File.dirname(__FILE__), 'support/**/*.rb')].sort.each { |f| require f }
